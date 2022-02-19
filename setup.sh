100f4b0bce9897d4d36e26289f4bfe16aa21e40f
Ariel-U <61334560+Ariel-U@users.noreply.github.com>
2022-02-19 14:47:28 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
-# crear bakups
 
+# crear bakups
 for file in $HOME/.{bashrc,bash_aliases,zshrc}
 do
     mv -v $file $HOME/*.bak
+done
 
+# copiar configuracion
 for file in ./home/.{bashrc,bash_aliases,zshrc} 
 do
     cp -v $file $HOME/
