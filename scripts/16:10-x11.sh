0cf472065a45ca60546a0c9dfe97db594b531dd1
Ariel-U <61334560+Ariel-U@users.noreply.github.com>
2023-12-30 08:12:40 -0300

    Update 16:10-x11.sh

--- scripts/16:10-x11.sh
+++ scripts/16:10-x11.sh
@@ -4,13 +4,13 @@
 # cvt [ancho] [alto] [frecuencia]
 # ej: cvt 1920 1200 60
 
-# agregar las resoluciones 16:10 más comunes
+# Agregar las resoluciones 16:10 más comunes
 xrandr --newmode 1920x1200_60.00  193.25  1920 2056 2256 2592  1200 1203 1209 1245 -hsync +vsync
 xrandr --newmode 1680x1050_60.00  146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
 xrandr --newmode 1440x900_60.00  106.50  1440 1528 1672 1904  900 903 909 934 -hsync +vsync
 xrandr --newmode 1280x800_60.00   83.50  1280 1352 1480 1680  800 803 809 831 -hsync +vsync
 
-# pasar las resoluciones al output HDMI-0 y VGA-0
+# Pasar las resoluciones al output HDMI-0 y VGA-0
 xrandr --addmode HDMI-0 1920x1200_60.00
 xrandr --addmode HDMI-0 1680x1050_60.00
 xrandr --addmode HDMI-0 1440x900_60.00
@@ -20,4 +20,5 @@ xrandr --addmode HDMI-0 1280x800_60.00
 #xrandr --addmode VGA-0 1440x900_60.00
 #xrandr --addmode VGA-0 1280x800_60.00
 
+# Ejecuta el cambio de resolución
 xrandr -s 1680x1050_60
