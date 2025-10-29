e2078fb4a585b4d63f474e99bce92d420f268b96
Ariel-U <61334560+Ariel-U@users.noreply.github.com>
2025-10-29 12:07:48 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 mkdir -v $HOME/.dotfiles
-mv -vr $HOME/setup/* $HOME/.dotfiles/
+mv -v $HOME/setup/* $HOME/.dotfiles/
 # crear bakups
 for file in $HOME/.{bashrc,aliases,zshrc,vimrc,tmux.conf,nanorc}
 do
